ba56f907395b79a57b25261fb2ae3fa229bff7cf
miller78130 <miller78130@gmail.com>
2021-04-27 20:47:26 -0500

    4-27 2100

---
+++ WeatherPy/config.py
@@ -0,0 +1,6 @@
+# gkey is Google
+gkey = "AIzaSyBhHfcm9VVxT-sSpAuHHSsRN4rITIbcufU"
+# ckey is census
+ckey = "06d3f750b9e458d7eb04280f9ee2512a1daa5f13"
+# wkey is Weather
+wkey = "053a3aa33379e4808ccbba98b02b75df"
